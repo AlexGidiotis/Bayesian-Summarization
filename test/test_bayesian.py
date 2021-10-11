54e79b7bf5e8121eabd4750f7feeec9a9df5e400
Alex Gidiotis <gidiotisAlex@outlook.com.gr>
2021-10-11 10:05:42 +0300

    testing bayesian generation

--- test/test_bayesian.py
+++ test/test_bayesian.py
@@ -1,8 +1,12 @@
+import os
+import tempfile
 import unittest
 import mock
 
 from transformers import AutoTokenizer, PegasusForConditionalGeneration, PegasusConfig
 
+from src.loaders import init_loader
+from test.test_loaders import write_test_json, create_test_loader
 from test.testing_common_utils import ids_tensor, floats_tensor, values_tensor
 from src.bayesian import BayesianSummarizer
 
@@ -23,14 +27,7 @@ class TestBayesianSummarizer(unittest.TestCase):
         self.bayesian_summarizer = BayesianSummarizer(model=self.model, tokenizer=self.tokenizer)
 
     def tearDown(self) -> None:
-        pass
-
-    def prepare_inputs(self):
-        input_ids = ids_tensor((self.batch_size * self.num_beams, self.sequence_length), self.vocab_size)
-        next_tokens = ids_tensor((self.batch_size, 2 * self.num_beams), self.vocab_size).to("cpu")
-        next_indices = ids_tensor((self.batch_size, 2 * self.num_beams), self.num_beams).to("cpu")
-        next_scores, _ = (-floats_tensor((self.batch_size, 2 * self.num_beams)).to("cpu")).sort(descending=True)
-        return input_ids, next_tokens, next_indices, next_scores
+        del self.bayesian_summarizer, self.tokenizer, self.model
 
     @mock.patch("transformers.PegasusForConditionalGeneration.generate")
     def test_mc_dropout(self, mock_generation):
@@ -57,3 +54,64 @@ class TestBayesianSummarizer(unittest.TestCase):
 
         self.assertEqual(generations[0][0], target_text)
         self.assertListEqual(input_ids[0].tolist(), mock_input_ids_tensor[0].tolist())
+
+    @mock.patch("transformers.PegasusForConditionalGeneration.generate")
+    def test_generate_bayesian(self, mock_generation):
+        mock_gen_ids = [[182, 117, 114, 3943, 5627, 107, 1]]
+        mock_gen_ids_tensor = values_tensor(mock_gen_ids)
+        args = {
+            "test_batch_size": 1,
+            "dataset_name": None,
+            "dataset_config_name": "",
+            "max_test_samples": 4,
+        }
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            test_loader = create_test_loader(args=args, tmp_dir=tmp_dir)
+
+            mock_generation.return_value = {
+                "sequences": mock_gen_ids_tensor,
+            }
+            generated_sums, target_sums, article_ids, bleuvars = self.bayesian_summarizer.generate_bayesian_summaries(
+                dataloader=test_loader,
+                device="cpu",
+                text_column="document",
+                summary_column="summary",
+                max_source_length=self.sequence_length,
+                num_beams=self.num_beams,
+                n=3)
+
+            self.assertEqual(len(generated_sums), args["max_test_samples"])
+            self.assertEqual(len(target_sums), args["max_test_samples"])
+            self.assertEqual(len(article_ids), args["max_test_samples"])
+            self.assertEqual(len(bleuvars), args["max_test_samples"])
+
+            del test_loader
+
+    @mock.patch("transformers.PegasusForConditionalGeneration.generate")
+    def test_generate_mc_summaries(self, mock_generation):
+        mock_gen_ids = [[182, 117, 114, 3943, 5627, 107, 1]]
+        mock_gen_ids_tensor = values_tensor(mock_gen_ids)
+        args = {
+            "test_batch_size": 1,
+            "dataset_name": None,
+            "dataset_config_name": "",
+            "max_test_samples": 4,
+        }
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            test_loader = create_test_loader(args=args, tmp_dir=tmp_dir)
+
+            mock_generation.return_value = {
+                "sequences": mock_gen_ids_tensor,
+            }
+            generated_sums = self.bayesian_summarizer.generate_mc_summaries(
+                test_loader,
+                device="cpu",
+                text_column="document",
+                max_source_length=self.sequence_length,
+                num_beams=self.num_beams,
+                n=3)
+
+            self.assertEqual(len(generated_sums), args["max_test_samples"])
+            self.assertEqual(len(generated_sums[0]), 3)
+
+            del test_loader
--- test/test_loaders.py
+++ test/test_loaders.py
@@ -16,26 +16,29 @@ def write_test_json(datafile, n):
             }, fp=jwf)
 
 
+def create_test_loader(args, tmp_dir):
+    datafile = os.path.join(tmp_dir, "test.json")
+    args["data_path"] = datafile
+    write_test_json(datafile, n=args["max_test_samples"])
+    test_loader = init_loader(
+        test_batch_size=args["test_batch_size"],
+        split="test", data_path=args["data_path"],
+        dataset_name=args["dataset_name"],
+        dataset_config_name=args["dataset_config_name"],
+        max_test_samples=args["max_test_samples"])
+    return test_loader
+
+
 class TestDataLoader(unittest.TestCase):
     def test_init_loader(self):
+        args = {
+            "test_batch_size": 1,
+            "dataset_name": None,
+            "dataset_config_name": "",
+            "max_test_samples": 4,
+        }
         with tempfile.TemporaryDirectory() as tmp_dir:
-            datafile = os.path.join(tmp_dir, "test.json")
-            args = {
-                "test_batch_size": 1,
-                "data_path": datafile,
-                "dataset_name": None,
-                "dataset_config_name": "",
-                "max_test_samples": 4,
-            }
-            write_test_json(datafile, n=4)
-
-            test_loader = init_loader(
-                test_batch_size=args["test_batch_size"],
-                split="test", data_path=args["data_path"],
-                dataset_name=args["dataset_name"],
-                dataset_config_name=args["dataset_config_name"],
-                max_test_samples=args["max_test_samples"])
-
+            test_loader = create_test_loader(args=args, tmp_dir=tmp_dir)
             self.assertEqual(len(test_loader), 4)
             del test_loader
 
@@ -82,4 +85,4 @@ class TestDataLoader(unittest.TestCase):
                 sample=[0, 2])
 
             self.assertEqual(len(test_loader), 2)
-            del test_loader
+            del test_loader, test_ds
