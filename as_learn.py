fb190e0a4891d006e83f1b4c5d9d8a4aad576bf0
Alex Gidiotis <gidiotisAlex@outlook.com.gr>
2021-10-18 10:45:32 +0300

    fix-logiing-levels

--- as_learn.py
+++ as_learn.py
@@ -4,36 +4,17 @@ import os
 import sys
 import random
 import time
-import re
 
 import torch
 
 from src.active_summarization import active_sum
 from src.common.loaders import init_dataset
-
+from src.common.logging_utils import set_global_logging_level
 
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
     datefmt="%m/%d/%Y %H:%M:%S",
     handlers=[logging.StreamHandler(sys.stdout)],)
-
-
-def set_global_logging_level(level=logging.ERROR, prefices=[""]):
-    """
-    Override logging levels of different modules based on their name as a prefix.
-    It needs to be invoked after the modules have been loaded so that their loggers have been initialized.
-
-    Args:
-        - level: desired level. e.g. logging.INFO. Optional. Default is logging.ERROR
-        - prefices: list of one or more str prefices to match (e.g. ["transformers", "torch"]). Optional.
-          Default is `[""]` to match all active loggers.
-          The match is a case-sensitive `module_name.startswith(prefix)`
-    """
-    prefix_re = re.compile(fr'^(?:{ "|".join(prefices) })')
-    for name in logging.root.manager.loggerDict:
-        if re.match(prefix_re, name):
-            logging.getLogger(name).setLevel(level)
-
 
 set_global_logging_level(logging.ERROR, ["transformers.configuration_utils"])
 logger = logging.getLogger()
